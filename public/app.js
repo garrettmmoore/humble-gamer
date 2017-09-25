74c50213e1b6a528c16c5eca02e9ea8f308e76f8
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-24 20:46:46 -0700

    Added navbar, added scrape button to fetch from database on click, and added start() function, renderEmpty() function, renderArticles() function, and handleArticleScrape() function to app.js

--- public/app.js
+++ public/app.js
@@ -1,12 +1,51 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      createNewOption(data[i])
-      // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+$(document).ready(function() {
+var articleContainer = $("#articles");
+$(document).on("click", ".scrape-new", handleArticleScrape);
+
+  // Once the page is ready, run the initPage function to kick things off
+  start();
+  
+  
+    function start() {
+      
+      // Empty the article container, run an AJAX request for any unsaved headlines
+      articleContainer.empty();
+      $.getJSON("/articles").then(function(data) {
+        // If we have headlines, render them to the page
+        if (data && data.length) {
+          renderArticles(data);
+
+        }
+        else {
+          // Otherwise render a message explaing we have no articles
+          renderEmpty();
+        }
+      });
     }
+
+function handleArticleScrape() {
+  // This function handles the user clicking any "scrape new article" buttons
+  $.get("/scrape").then(function(data) {
+    // If we are able to succesfully scrape the NYTIMES and compare the articles to those
+    // already in our collection, re render the articles on the page
+    // and let the user know how many unique articles we were able to save
+    setTimeout(function(){ start(); }, 1000);
+    // bootbox.alert("<h3 class='text-center m-top-80'>" + data.message + "<h3>");
   });
+}
+
+// Grab the articles as a json
+function renderArticles(articles) {
+  $.getJSON("/articles", function(data) {
+      // For each one
+      for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        createNewOption(data[i])
+        // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      }
+  });
+}
+
 
   function createNewOption(data){
     var newCardDeck = $("<div class='card-deck'>");
@@ -29,7 +68,7 @@ $.getJSON("/articles", function(data) {
     return newCardDiv;
 };
 
-  // Whenever someone clicks a p tag
+  // Whenever someone clicks a comment button
   $(document).on("click", "#comment", function() {
     // Empty the notes from the note section
     $("#notes").empty();
@@ -91,3 +130,27 @@ $.getJSON("/articles", function(data) {
     $("#titleinput").val("");
     $("#bodyinput").val("");
   });
+
+  function renderEmpty() {
+    // This function renders some HTML to the page explaining we don't have any articles to view
+    // Using a joined array of HTML string data because it's easier to read/change than a concatenated string
+    var emptyAlert = $(
+      [
+        "<div class='alert alert-warning text-center'>",
+        "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
+        "</div>",
+        "<div class='panel panel-default'>",
+        "<div class='panel-heading text-center'>",
+        "<h3>What Would You Like To Do?</h3>",
+        "</div>",
+        "<div class='panel-body text-center'>",
+        "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
+        "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
+        "</div>",
+        "</div>"
+      ].join("")
+    );
+    // Appending this data to the page
+    articleContainer.append(emptyAlert);
+  }
+});
